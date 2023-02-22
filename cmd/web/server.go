9f794da2fd3e5557b219e98157ada96ffe9ecdc9
TupikDenis <tdenis.20012510@gmail.com>
2023-02-22 13:07:54 +0300

    add cors

--- cmd/web/server.go
+++ cmd/web/server.go
@@ -1,8 +1,24 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"time"
+)
 
 func startServer(router *gin.Engine, port string) {
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"https://foo.com"},
+		AllowMethods:     []string{"PUT", "PATCH"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "https://github.com"
+		},
+		MaxAge: 12 * time.Hour,
+	}))
+
 	err := router.Run(port)
 
 	if err != nil {
