f8f9336b0f7c0218ed7496a49812ee18b19e0bdc
TupikDenis <tdenis.20012510@gmail.com>
2023-02-24 11:07:41 +0300

    fixed cors 3

--- cmd/web/server.go
+++ cmd/web/server.go
@@ -3,10 +3,21 @@ package main
 import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"time"
 )
 
 func startServer(router *gin.Engine, port string) {
-	router.Use(cors.Default())
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"*"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "https://github.com"
+		},
+		MaxAge: 12 * time.Hour,
+	}))
 
 	err := router.Run(port)
 
