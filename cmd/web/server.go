9213bc27766a19f713febdec77d1106be724dd1b
TupikDenis <tdenis.20012510@gmail.com>
2023-02-24 11:20:30 +0300

    fixed cors 7

--- cmd/web/server.go
+++ cmd/web/server.go
@@ -1,16 +1,10 @@
 package main
 
 import (
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func startServer(router *gin.Engine, port string) {
-	config := cors.DefaultConfig()
-	config.AllowAllOrigins = true
-	config.AllowHeaders = []string{"PUT", "POST", "DELETE", "GET"}
-	router.Use(cors.New(config))
-
 	err := router.Run(port)
 
 	if err != nil {
