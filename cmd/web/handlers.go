72df7447debc01925c6a00ec7b659a7f53302608
TupikDenis <tdenis.20012510@gmail.com>
2023-02-24 11:23:19 +0300

    fixed cors 8

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,34 +1,16 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func handle() {
 	router := createRouter()
-	router.Use(CORSMiddleware())
 	userRouterGroup(router)
 	subjectRouterGroup(router)
 	markRouterGroup(router)
 	router.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	startServer(router, ":8080")
-}
-
-func CORSMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
-
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
-			return
-		}
-
-		c.Next()
-	}
 }
--- cmd/web/routers.go
+++ cmd/web/routers.go
@@ -6,7 +6,25 @@ import (
 )
 
 func createRouter() *gin.Engine {
-	return gin.Default()
+	router := gin.Default()
+	router.Use(cORSMiddleware())
+	return router
+}
+
+func cORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, DELETE, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
 
 func userRouterGroup(router *gin.Engine) {
